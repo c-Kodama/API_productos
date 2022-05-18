b18e732808b98a67d4e689ff99b5869ff5a90924
Constanza Cifuentes <con.cifuentesc@duocuc.cl>
2022-05-18 17:43:31 -0400

    metodo listar productos, y metodo obtener producto por codigo

--- rest_productos/urls.py
+++ rest_productos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from rest_productos.views import lista_categorias
+from rest_productos.views import lista_categorias, lista_productos, obtenerProducto
 
 urlpatterns = [
     path('categorias/', lista_categorias, name='lista de categorias'),
+    path('productos/', lista_productos, name='lista de productos'),
+    path('producto/', obtenerProducto, name='obtener un producto por codigo'),
+
 ]
--- rest_productos/views.py
+++ rest_productos/views.py
@@ -9,10 +9,29 @@ from .serializers import productoSerializer, subCategoriaSerializer, CategoriaSe
 
 # Create your views here.
 
+#Listar Categorias
 @csrf_exempt
 @api_view(['GET'])
 def lista_categorias(request):
     if request.method == 'GET':
         categorias = Categoria.objects.all()
         serializer = CategoriaSerializer(categorias, many=True)
+        return Response(serializer.data)
+
+#Obtener pruductos
+@csrf_exempt    
+@api_view(['GET'])
+def lista_productos(request):
+    if request.method == 'GET':
+        productos = producto.objects.all()
+        serializer = productoSerializer(productos, many=True)
+        return Response(serializer.data)
+
+#obtener un producto
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def obtenerProducto(request):
+    if request.method == 'POST':
+        productoCodigo = producto.objects.get(codigo=request.data.get("codigo"))
+        serializer = productoSerializer(productoCodigo)
         return Response(serializer.data)
